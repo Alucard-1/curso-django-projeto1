bfff5e6892487ec73d490ae35a4be5291df05d0a
joao <joaoantonio1991.ja@gmail.com>
2024-03-18 17:17:44 -0300

    finalizando cards

---
+++ recipes/views.py
@@ -0,0 +1,18 @@
+
+from django.shortcuts import render
+
+
+
+def home(request):
+    return render(request, 'recipes/pages/home.html',context={
+        'name':'joao',
+    })
+
+
+
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html',context={
+        'name':'joao',
+    })
+
